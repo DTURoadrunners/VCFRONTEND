fc1eaeb3446a501a48618973a2f58e5cf24e7f7c
Thomas Bech Madsen <thomasbm@live.dk>
2018-01-04 12:37:03 +0100

    Components tab almost done, cant figure out how to make accords dynamic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AlertModule } from 'ngx-bootstrap/alert'; // load bootstrap alert
 import { TabsModule } from 'ngx-bootstrap/tabs'; // load bootstrap tabs
 import { ButtonsModule } from 'ngx-bootstrap/buttons'; // load bootstrap buttons
 import { AccordionModule } from 'ngx-bootstrap/accordion'; // load accordion list (expanding list)
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'; // load to work with angularForms
 import { AppComponent } from './app.component';
@@ -59,7 +61,9 @@ import { ComponentsComponent } from './componenttype/components/components.compo
     TabsModule.forRoot(),
     ButtonsModule.forRoot(),
     AppRoutingModule,
-    AccordionModule.forRoot()
+    AccordionModule.forRoot(),
+    ModalModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   exports: [BsDropdownModule],
   providers: [AuthService, AuthGuard, CnAuthService],
--- src/app/componenttype/components/components.component.ts
+++ src/app/componenttype/components/components.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { COMPONENTS } from './../../mock/mock-components'
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @Component({
   selector: 'app-components',
@@ -7,12 +11,24 @@ import { COMPONENTS } from './../../mock/mock-components'
   styleUrls: ['./components.component.css']
 })
 export class ComponentsComponent implements OnInit {
-
+  alerts: any = [];
   components = COMPONENTS;
+  modalRef: BsModalRef;
+  componentForm: FormGroup; //call it with [formGroup]="componentForm" in the HTML
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,         // inject the formbuilder
+    private modalService: BsModalService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.componentForm = this.fb.group({
+      status: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
   }
-  
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
