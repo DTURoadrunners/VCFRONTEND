63bda462d4762a25bfd4f0ef51d7625a0e676a14
Thomas Bech Madsen <thomasbm@live.dk>
2018-01-04 10:57:09 +0100

    Continued work on component

--- src/app/componenttype/components/components.component.ts
+++ src/app/componenttype/components/components.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { COMPONENTS } from './../../mock/mock-components'
 
 @Component({
   selector: 'app-components',
@@ -7,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsComponent implements OnInit {
 
+  components = COMPONENTS;
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  log(event: boolean) {
-    console.log(`Accordion has been ${event ? 'opened' : 'closed'}`);
-  }
+  
 }
---
+++ src/app/models/component.ts
@@ -0,0 +1,8 @@
+
+export class Component {
+  id: number;
+  status: string;
+  comment: string;
+  timestamp: Date;
+}
+
