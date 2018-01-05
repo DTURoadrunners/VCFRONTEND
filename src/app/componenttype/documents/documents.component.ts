a99c5eda8f450eaf3c17d77e19c7c47f233ac0f0
Thomas Bech Madsen <thomasbm@live.dk>
2018-01-05 10:16:01 +0100

    Added more buttons and stuff

--- src/app/componenttype/documents/documents.component.ts
+++ src/app/componenttype/documents/documents.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { DOCUMENTS } from '../../mock/mock-documents'; 
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 
 
 
@@ -10,14 +11,20 @@ import { DOCUMENTS } from '../../mock/mock-documents';
 })
 export class DocumentsComponent implements OnInit {
 
-  
+  modalRef: BsModalRef;
 
   mydocuments = DOCUMENTS;
 
-  constructor() { 
+  constructor(
+    private modalService: BsModalService
+  ) { 
   }
 
   ngOnInit() {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
