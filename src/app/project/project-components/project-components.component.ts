a262cc5d7e5d063a577d5dc6f0010ba58d0d8b7c
Clusive <rdobrandelev@gmail.com>
2018-01-05 09:21:49 +0100

    Did more work on the project page

--- src/app/project/project-components/project-components.component.ts
+++ src/app/project/project-components/project-components.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { COMPONENTS } from '../../mock/mock-components';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-project-components',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponentsComponent implements OnInit {
 
-  constructor() { }
+  alerts: any = [];
+  components = COMPONENTS;
+  modalRef: BsModalRef;
+  componentForm: FormGroup; //call it with [formGroup]="componentForm" in the HTML
+  selectedComponent: Component;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,         // inject the formbuilder
+    private modalService: BsModalService
+  ) { }
+
+  ngOnInit(): void {
+    this.componentForm = this.fb.group({
+      status: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
+  }
+
+  openModal(template: TemplateRef<any>, component: Component) {
+    this.modalRef = this.modalService.show(template);
+    this.selectedComponent = component;
   }
 
 }
