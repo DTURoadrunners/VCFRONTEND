639780fa299cf6663774f3880da84b76be27a732
Clusive <rdobrandelev@gmail.com>
2018-01-05 13:37:34 +0100

    Started work on the create new project function

--- src/app/project/myprojects/myprojects.component.ts
+++ src/app/project/myprojects/myprojects.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from "@angular/router";
 import { PROJECTS } from "./../../mock/mock-projects";
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-myprojects',
@@ -9,14 +12,28 @@ import { PROJECTS } from "./../../mock/mock-projects";
 })
 export class MyprojectsComponent implements OnInit {
   projects = PROJECTS;
+  modalRef: BsModalRef;
+  projectForm: FormGroup; //call it with [formGroup]="componentForm" in the HTML
+  selectedProjectt: Project;
 
-  constructor(private router: Router) { }
-
+  constructor(
+  private router: Router, 
+  private fb: FormBuilder,       // inject the formbuilder
+  private modalService: BsModalService
+  ){}
+  
   ngOnInit() {
+    this.projectForm = this.fb.group({
+      description: [''], //Optional
+      comment: [''], //Optional
+    });
   }
 
   cardClick(id: number) {
     this.router.navigate(["/project/"+id]);
   }
-
+  openModal(template: TemplateRef<any>, project: Project) {
+    this.modalRef = this.modalService.show(template);
+    this.selectedProject = project;
+  }
 }
