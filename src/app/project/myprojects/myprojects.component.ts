91fe118a25bc452b1829d359eaae90c1ffd2f0ae
mrdanielpc <danielplaetner@gmail.com>
2018-01-08 08:14:14 +0100

    merge

--- src/app/project/myprojects/myprojects.component.ts
+++ src/app/project/myprojects/myprojects.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { PROJECTS } from "./../../mock/mock-projects";
+import { Project } from '../../models/project';
+import { ProjectService } from '../../service/project.service';
+
 
 @Component({
   selector: 'app-myprojects',
@@ -8,15 +10,27 @@ import { PROJECTS } from "./../../mock/mock-projects";
   styleUrls: ['./myprojects.component.css']
 })
 export class MyprojectsComponent implements OnInit {
-  projects = PROJECTS;
+  projects: Project[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.getProjects();
+    this.createProject();
+    this.createProject();
   }
 
   cardClick(id: number) {
     this.router.navigate(["/project/"+id]);
+  }
+
+
+  getProjects(): void {
+    this.projectService.getProjects().subscribe(projects => this.projects = projects);
+  }
+
+  createProject(): void{
+    this.projectService.createProject(  { id: 4, name: "VCFRONTEND", description: "Her udvikles frontend til VCAPI" }).subscribe(projects => this.projects = projects);
   }
 
 }
