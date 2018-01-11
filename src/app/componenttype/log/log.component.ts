00b153ca16fc2da5a9c566dfb7b0a74187a05893
mrdanielpc <danielplaetner@gmail.com>
2018-01-11 08:20:51 +0100

    finished log

--- src/app/componenttype/log/log.component.ts
+++ src/app/componenttype/log/log.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { COMPONENTTYPELOG } from '../../mock/mock-componTypeLog';
 
+// modal
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+// log
+import { ComponentTypeLog } from '../../models/log/componentTypeLog'; 
+import { LogService } from '../../service/log.service'; 
 
 @Component({
   selector: 'app-log',
@@ -13,17 +20,27 @@ export class LogComponent implements OnInit {
   totalItems = this.logdata.length;
   currentPage = 1;
   smallnumPages = 0;
+  modalRef: BsModalRef;
+  selectedComponentTypeLog: ComponentTypeLog;
 
-  constructor() { }
+  constructor(
+    private modalService: BsModalService,
+    private logService: LogService
+
+  ) { }
 
   ngOnInit() {
-    console.log(this.logdata.length)
   }
 
   
  
   setPage(pageNo: number): void {
     this.currentPage = pageNo;
+  }
+
+  openModal(template: TemplateRef<any>, componentTypeLog: ComponentTypeLog) {
+    this.modalRef = this.modalService.show(template);
+    this.selectedComponentTypeLog = componentTypeLog;
   }
 
 }
