b8ddc536bdd52dcd4eaafcbc7715cb01abcfbbe9
mrdanielpc <danielplaetner@gmail.com>
2018-01-17 10:32:07 +0100

    added comment to string length pipe

--- src/app/pipe/stringlength.pipe.ts
+++ src/app/pipe/stringlength.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StringlengthPipe implements PipeTransform {
 
+  /**
+   * returns the input string, if the input is longer then the limit add '...' after the input
+   * @param value define the input string {{string | stringlength: limit (number)}}
+   * @param limit define how long the string should before the cut {{ string | stringlength: limit (number) }} ex {{data| stringlength: 50}}
+   */
   transform(value: string, limit: number): string {
-    
     return value.length > limit ? value.substring(0, limit) + '...' : value;
   }
 
