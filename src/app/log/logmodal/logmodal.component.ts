0d8220177b0ad66c76d0d76f7d7d9644f3315c11
mrdanielpc <danielplaetner@gmail.com>
2018-01-17 09:13:32 +0100

    added comments to the log modal

--- src/app/log/logmodal/logmodal.component.ts
+++ src/app/log/logmodal/logmodal.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
+// import bootstrap modal modules
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
+// import Log model and the needed service 
 import { LogEntry } from '../../models/LogEntry';
 import { LogService } from '../../service/log.service';
 
@@ -13,9 +15,9 @@ import { LogService } from '../../service/log.service';
 })
 export class LogmodalComponent implements OnInit {
 
-  @Input('log') log: any;
-  @Input('logModal') modalRef: BsModalRef;
-  modalRefChild: BsModalRef;
+  @Input('log') log: any; // inheritence from the parrent component, called with [log] = data
+  @Input('logModal') modalRef: BsModalRef; // inheritence from the parrent component, called with [logModal] = data
+  modalRefChild: BsModalRef; // reference for the modal 
 
   constructor(
     private modalService: BsModalService
@@ -25,19 +27,33 @@ export class LogmodalComponent implements OnInit {
 
   }
 
+  /**
+   * hide the child modal
+   */
   hideChildModal(): void {
     this.modalRef.hide();
   }
 
+  /**
+   * open the modal with the corresponding HTML template
+   * @param template reference to the NG HTML template
+   */
   openModal(template: TemplateRef<any>) {
     this.modalRefChild = this.modalService.show(template, {class: 'modal-sm'});
   }
  
+  /**
+   * click event, will close the HTML modal
+   * TODO: rollback the event 
+   */
   confirm(): void {
   
     this.modalRefChild.hide();
   }
  
+  /**
+   * click event, will close the HTML modal
+   */
   decline(): void {
 
     this.modalRefChild.hide();
