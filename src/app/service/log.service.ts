d685ae06c44830f9d8fc32e4aec821b17047fe14
mrdanielpc <danielplaetner@gmail.com>
2018-01-17 13:09:12 +0100

    added comment to log service

--- src/app/service/log.service.ts
+++ src/app/service/log.service.ts
@@ -9,39 +9,68 @@ import { COMPONENTTYPELOG } from '../mock/mock-componTypeLog';
 import { COMPONENTLOG } from '../mock/mock-componentLog';
 import { DOCUMENTLOG } from '../mock/mock-documentLog'; 
 
+/**
+ * Handles all the log for each different process
+ */
 @Injectable()
 export class LogService {
 
   constructor() { }
 
   //PROJECT
+
+  /**
+   * return the logentry
+   * @param id  id of the logentry
+   */
   getProjectLog(id: number): Observable<LogEntry>{
     return of(PROJECTLOG.find(projectlog => projectlog.id === id));
   }
 
+  /**
+   * return all the logentries
+   */
   getProjectLogs(): Observable<LogEntry[]> {
     return of(PROJECTLOG);
   }
 
+  /**
+   * get the 7 latest log entries
+   */
   getLatestProjectLog(): Observable<LogEntry[]> {
     return of(PROJECTLOG.slice(PROJECTLOG.length - 7, PROJECTLOG.length));
   }
 
   //COMPONENTTYPE
+
+  /**
+   * get the 7 latest logentries
+   */
   getLatestComponettypeLog(): Observable<LogEntry[]> {
     return of(COMPONENTTYPELOG.slice(COMPONENTTYPELOG.length - 7, COMPONENTTYPELOG.length));
   }
 
+  /**
+   * get the log entries
+   */
   getComponenttypeLogs(): Observable<LogEntry[]> {
     return of(COMPONENTTYPELOG);
   }
 
   //COMPONENT
+  /**
+   * return the logentries
+   * @param componentId componentid of the component log
+   */
   getComponentLogs(componentId: number): Observable<LogEntry[]> {
     return of(COMPONENTLOG);
   }
 
   //DOCUMENT
+  /**
+   * get the log of document id based on the log id
+   * @param documentId document id of the logentry
+   */
   getDocumentLogs(documentId: number): Observable<LogEntry[]> {
     return of(DOCUMENTLOG);
   }
